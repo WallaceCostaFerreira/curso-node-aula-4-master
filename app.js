2f00d037f2f88b9c56dc04d75278e79094b64659
Wallace Costa Ferreira <80697255+WallaceCostaFerreira@users.noreply.github.com>
2021-06-18 21:11:57 -0300

    Finalização curso node

---
+++ app.js
@@ -0,0 +1,85 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var flash = require('express-flash');
+var session = require('express-session');
+// var mysql = require('mysql');
+var connection = require('./lib/db');
+
+// admin-bro - setup
+const AdminBro = require('admin-bro');
+const AdminBroExpress = require('@admin-bro/express');
+const AdminBroSequelize = require('@admin-bro/sequelize');
+
+const Posts = require('./model/posts');
+
+AdminBro.registerAdapter(AdminBroSequelize);
+
+var apiRouter = require('./routes/restmob');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// admin-bro options
+const adminBro = new AdminBro({
+  database: [connection],
+  rootPah: '/admin',
+  resources: [
+    {
+      resource: Posts, options: {
+        properties: {
+          content: { type: 'richtext'},
+        },
+      },
+    },
+  ],
+})
+
+const router = AdminBroExpress.buildRouter(adminBro)
+app.use(adminBro.options.rootPah, router)
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  cookie: { maxAge: 60000 },
+  store: new session.MemoryStore,
+  saveUninitialized: true,
+  resave: true,
+  secret: 'myapptoken'
+}))
+
+app.use(flash());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
